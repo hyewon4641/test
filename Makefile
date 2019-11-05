5c47a4c9b53e1ecff893e4a3f331e58e99b26552
hyewon <phw4641@mju.ac.kr>
2019-11-05 17:19:05 +0900

    make

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:final.elf
 
-final.elf:1.o 2.o 3.o main.o
+final.elf:1.o 2.o 3.o 
 	gcc -o final.elf 1.o 2.o 3.o
 
 1.o:1.c myProject.h
@@ -12,8 +12,7 @@ final.elf:1.o 2.o 3.o main.o
 3.o:3.c myProject.h
 	gcc -c 3.c
 
-main.o:main.c myproject.h
-	gcc -c main.c
+
 clean:
 	rm final.elf 1.o 2.o 3.o
 
--- myProject.h
+++ myProject.h
@@ -1,17 +1,5 @@
 #include <stdio.h>
 
-int printMyName1(void)
-{
-	printf("my name is [hong]\n");
-	return 1;
-}
-int printMyName2(void)
-{
-	printf("my name is [seunggyun]\n");
-	return 1;
-}
-int printMyName3(void)
-{
-	printf("my name is [moon]\n");
-	return 1;
-}
+int printMyName1(void);
+int printMyName2(void);
+int printMyName3(void);
