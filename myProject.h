056c2f6fd96027ae1abc5c27f8d411951b0e851b
hyewon <phw4641@mju.ac.kr>
2019-11-05 16:26:33 +0900

    h file

---
+++ myProject.h
@@ -0,0 +1,16 @@
+
+int printMyName1(void)
+{
+	printf("my name is [hong]\n");
+	return 1;
+}
+int printMyName2(void)
+{
+	printf("my name is [seunggyun]\n");
+	return 1;
+}
+int printMyName3(void)
+{
+	printf("my name is [moon]\n");
+	return 1;
+}
