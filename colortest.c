f30ffc93e6cd47ededcbddc6c1719d11d57eb189
hyewon <phw4641@mju.ac.kr>
2019-12-02 07:37:32 +0900

    led

--- colortest.c
+++ colortest.c
@@ -1,18 +1,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <linux/input.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/msg.h>
-#include <ctype.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include "colorledtest.h"
 
 int main(void)
 {
